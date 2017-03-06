a56bf2695bcf1a8462e00346406a343ec5d45747
manggala <dmazter.mgm@gmail.com>
2017-03-06 12:58:45 +0700

    Rest Note Bug Resolve

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,14 +32,14 @@ class HomeController extends Controller
     }
 
     public function restNote(){
-        $data['notes'] = Note::orderBy('deadline', 'asc')->get();
+        $data['notes'] = Note::where('user_id', Auth::user()->id)->orderBy('deadline', 'asc')->get();
         $data['newNotes'] = [];
         foreach ($data['notes'] as $note) {
             array_push($data['newNotes'], [
                 'id' => $note->id,
                 'title' => $note->title,
                 'content' => $note->content,
-                'deadline' => $note->deadline,
+                'deadline' => $note->deadline,user
                 'is_done' => $note->is_done == true ? 1 : 0,
                 'is_alerted' => $note->is_alerted == true ? 1 : 0,
                 'label_id' => $note->label_id,
